c85ecf4b043e2ab551b0c79f467d1c0486e318cb
QF2026 <276910026+QF2026@users.noreply.github.com>
2026-05-21 17:04:10 +0800

    更新 diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,6 +9,6 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
-cd openwrt
+
 #更改默认IP
 sed -i 's/192.168.1.1/192.168.1.200/g' package/base-files/files/bin/config_generate
